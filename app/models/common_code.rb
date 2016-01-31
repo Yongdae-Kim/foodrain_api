b5384bc4fe3e4bed909bd78f95f47985a927cc00
Yongdae Kim <yongdae91@hanmail.net>
2016-01-31 13:50:55 +0900

    공통코드 검색 쿼리 추가

--- app/models/common_code.rb
+++ app/models/common_code.rb
@@ -1,5 +1,13 @@
 class CommonCode < ApplicationRecord
   def self.find_by_code(main_code, detail_id)
-    where(main_code: main_code).where(detail_id: detail_id).first.name_kor
+    find_by_main_code(main_code).find_by_detail_id(detail_id)
+  end
+
+  def self.find_by_main_code(main_code)
+    where(main_code: main_code)
+  end
+
+  def self.find_by_detail_id(detail_id)
+    where(detail_id: detail_id).first.name_kor
   end
 end
