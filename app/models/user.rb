0bad9be8eb2d02546dd3e057bc271f1369bbffc6
Yongdae Kim <yongdae91@hanmail.net>
2016-02-01 20:56:59 +0900

    사용자 인증 모델 추가

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,15 @@
 class User < ApplicationRecord
+  include UsersHelper
+
+  after_create :gen_user_auth
+
   has_one :image, as: :imageable
   has_many :user_auths
   has_many :reviews
+
+  private
+
+  def gen_user_auth
+    UsersHelper.gen_user_auth(self)
+  end
 end
