03092b86030ecc10a1d2f9a95a19ac3465631e78
Yongdae Kim <yongdae91@hanmail.net>
2016-02-03 01:55:54 +0900

    리뷰 모델 검색 쿼리 수정

--- app/models/review.rb
+++ app/models/review.rb
@@ -7,13 +7,13 @@ class Review < ApplicationRecord
   belongs_to :user, counter_cache: true
   has_many :images, as: :imageable
 
-  scope :find_by_store, lambda { |store_id|
+  def self.find_by_store(store_id)
     where(store_id: store_id.to_i) if store_id.present?
-  }
+  end
 
-  scope :find_by_user, lambda { |user_id|
+  def self.find_by_user(user_id)
     where(user_id: user_id.to_i) if user_id.present?
-  }
+  end
 
   private
 
